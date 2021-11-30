fda0d0734c75df395093338258a66d376d941d9e
Daniel Lawrence <danlaw@amazon.com>
2021-11-29 21:16:02 -0500

    Initial setup

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ extern "C" long long asm_foo(void);
 
 //There's a small library of useless functions defined in assembly called library.asm and
 //a header full of corresponding C extern symbols called library.hpp
-#include "library.hpp"
+#include "lib/library.hpp"
 
 int main()
 {
---
+++ tst/test.cpp
@@ -0,0 +1,26 @@
+#include "../lib/library.hpp"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+extern "C" long long asm_foo(void);
+
+TEST_CASE("Testing assembly functions"){
+    SUBCASE("asm_is_windows"){
+        CHECK(!asm_is_windows());
+    }
+    SUBCASE("compute answer"){
+        compute_answer();
+        CHECK(compute_answer() == 42);
+    }
+    SUBCASE("increment pointer"){
+        int64_t not_zero = 0;
+        increment_pointer_int64(&not_zero);
+        CHECK(not_zero != 0);
+    }
+    SUBCASE("asm_foo"){
+        CHECK(asm_foo() == 1337);
+    }
+    SUBCASE("sum of four ints"){
+        CHECK(sum_4_ints(1, 1, 1, 1) == 4);
+    }
+}
